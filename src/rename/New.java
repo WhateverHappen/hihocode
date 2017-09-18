add765e9e306e5db165a9150753cd40890f7b6f4
嘲讽脸 <346203645@qq.com>
2017-09-18 11:33:43 +0800

    learn about branch

---
+++ src/rename/New.java
@@ -0,0 +1,11 @@
+package rename;
+
+/**
+ * Created by thanatos on 2017/9/18.
+ */
+public class New {
+    public static void main(String args[]){
+        String test = "i just try to learn about git-branch";
+
+    }
+}
